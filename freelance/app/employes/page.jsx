cbf72dfd51be42b58b86338efa7f9ffd8db69dce
salif18 <salifmoctarkonate@gmail.com>
2025-03-08 21:41:29 +0000

    ff

--- freelance/app/employes/page.jsx
+++ freelance/app/employes/page.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import LayoutPage from '../layouts/Layout'
+import styles from "../styles/_employe.module.scss"
 
 export default function page() {
   return (
-    <main>
-      
-    </main>
+    <LayoutPage>
+      <main className={styles.employe}>
+       <section className={styles.banner} >
+        <h2>Les catégories ci-dessous détaillent ce savoir-faire</h2>
+       </section>
+       <section className={styles.listCategorie}>
+         <h1>Candidats professionnels</h1>
+         <ul className={styles.CategorieContainer}>
+         {
+         [1,2,3,4,5,6,7,8].map((a,index) =>(
+           <li id={index}>
+            <section className={styles.categorie}>
+              <figure>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjh8FtIDVKI6npdhfevlj-1mXYzIZC71B-IA&s' />
+              </figure>
+              <h2>Design</h2>
+            </section>
+           </li>
+          ))
+         }
+         </ul>
+       </section>
+       <section className={styles.listCategorie}>
+         <h1>Les categories les plus recherchées</h1>
+         <ul className={styles.CategorieContainer}>
+         {
+         [1,2].map((a) =>(
+           <li id={a}>
+            <section className={styles.categorie}>
+              <figure>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjh8FtIDVKI6npdhfevlj-1mXYzIZC71B-IA&s' />
+              </figure>
+              <h2>Design</h2>
+            </section>
+           </li>
+          ))
+         }
+         </ul>
+       </section>
+      </main>
+    </LayoutPage>
   )
 }
--- freelance/app/ui/QuelquesOffres.jsx
+++ freelance/app/ui/QuelquesOffres.jsx
@@ -8,29 +8,26 @@ function QuelquesOffres() {
   const _offreList = [
     {
       _id:1,
-      name:"Amadou",
+      name:"Hervé",
       img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRlgUtBQrgA8rzPYDpXnr02e8OFVQLBuLQcw&s",
-      proffession:"Clinique Polytechnique",
-      categorie_offre:"Santé",
-      sujet:"A la recherche d'un technicien de santé pour deux postes",
+      categorie_offre:"Informatiques",
+      sujet:"A la recherche de deux informaticiens pour deux postes",
       candidatures:0,
       date:"07/03/2025"
     },
     {
       _id:2,
-      name:"Amadou",
+      name:"BKSHOP",
       img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRlgUtBQrgA8rzPYDpXnr02e8OFVQLBuLQcw&s",
-      proffession:"Clinique Polytechnique",
-      categorie_offre:"Santé",
-      sujet:"A la recherche d'un technicien de santé pour deux postes",
+      categorie_offre:"Developpeur",
+      sujet:"A la recherche d'un developpeur",
       candidatures:0,
       date:"07/03/2025"
     },
     {
       _id:3,
-      name:"Amadou",
+      name:"Clinique Polytechnique",
       img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRlgUtBQrgA8rzPYDpXnr02e8OFVQLBuLQcw&s",
-      proffession:"Clinique Polytechnique",
       categorie_offre:"Santé",
       sujet:"A la recherche d'un technicien de santé pour deux postes",
       candidatures:0,
@@ -40,9 +37,8 @@ function QuelquesOffres() {
       _id:4,
       name:"Amadou",
       img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRlgUtBQrgA8rzPYDpXnr02e8OFVQLBuLQcw&s",
-      proffession:"Clinique Polytechnique",
-      categorie_offre:"Santé",
-      sujet:"A la recherche d'un technicien de santé pour deux postes",
+      categorie_offre:"Redacteur",
+      sujet:"A la recherche d'un redacteur",
       candidatures:0,
       date:"07/03/2025"
     },
