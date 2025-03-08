6f04b0602d65d8e7f00eaa698ce939ae264726bc
salif18 <salifmoctarkonate@gmail.com>
2025-03-08 18:41:26 +0000

    ajout de carte offre

--- freelance/app/components/OffreCard.js
+++ freelance/app/components/OffreCard.js
@@ -2,10 +2,26 @@
 
 import React from 'react'
 import styles from '../styles/_offreCard.module.scss'
-function OffreCard() {
+function OffreCard({item}) {
   return (
     <section className={styles.cardOffre}>
-      
+      <section className={styles.row}>
+      <section className={styles.profil}>
+       <figure>
+        <img src={item.img} alt='' />
+       </figure>
+     
+         <p>{item.name}</p>
+       
+     </section>
+   
+        <p>{item.date}</p>
+      </section>
+      <section className={styles.column}>
+       <h2>{item.categorie_offre}</h2>
+       <p>{item.sujet}</p>
+       <p>Candidatures({item.candidatures})</p>
+      </section>
     </section>
   )
 }
--- freelance/app/ui/QuelquesOffres.jsx
+++ freelance/app/ui/QuelquesOffres.jsx
@@ -1,51 +1,54 @@
 import React from 'react'
 import PrestataireCard from '../components/PrestataireCard'
 import styles from "../styles/_quelques_offres.module.scss"
+import OffreCard from '../components/OffreCard'
 
 function QuelquesOffres() {
 
-  const PrestataireList = [
+  const _offreList = [
     {
       _id:1,
       name:"Amadou",
       img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRlgUtBQrgA8rzPYDpXnr02e8OFVQLBuLQcw&s",
-      proffession:"Technicien sante"
+      proffession:"Clinique Polytechnique",
+      categorie_offre:"Santé",
+      sujet:"A la recherche d'un technicien de santé pour deux postes",
+      candidatures:0,
+      date:"07/03/2025"
     },
     {
       _id:2,
       name:"Amadou",
       img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRlgUtBQrgA8rzPYDpXnr02e8OFVQLBuLQcw&s",
-      proffession:"Technicien sante"
+      proffession:"Clinique Polytechnique",
+      categorie_offre:"Santé",
+      sujet:"A la recherche d'un technicien de santé pour deux postes",
+      candidatures:0,
+      date:"07/03/2025"
     },
     {
       _id:3,
       name:"Amadou",
       img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRlgUtBQrgA8rzPYDpXnr02e8OFVQLBuLQcw&s",
-      proffession:"Technicien sante"
+      proffession:"Clinique Polytechnique",
+      categorie_offre:"Santé",
+      sujet:"A la recherche d'un technicien de santé pour deux postes",
+      candidatures:0,
+      date:"07/03/2025"
     },
-    {
-      _id:4,
-      name:"Amadou",
-      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRlgUtBQrgA8rzPYDpXnr02e8OFVQLBuLQcw&s",
-      proffession:"Technicien sante"
-    },
-    {
-      _id:5,
-      name:"Amadou",
-      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRlgUtBQrgA8rzPYDpXnr02e8OFVQLBuLQcw&s",
-      proffession:"Technicien sante"
-    }
+    
+    
   ]
   return (
     <section className={styles.quelquesOffres}>
     <h1>Les offres d'emploi &#8594;</h1>
     
     <section className={styles.row}>
-    {PrestataireList
+    {_offreList
         .slice(0, 6)
         
         .map((item) => (
-          <PrestataireCard item={item} key={item._id} />
+          <OffreCard item={item} key={item._id} />
         ))}
     </section>
 
